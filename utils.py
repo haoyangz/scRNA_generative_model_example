7fbbf0494d6fbd512135c1292815d92fc9dafdd6
haoyangz <zenghaoyang@gmail.com>
2024-08-08 22:41:10 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import numpy as np
 import torch
+from collections import defaultdict
+from sklearn.metrics import average_precision_score, roc_auc_score
+import pandas as pd
 
 
 # https://github.com/scverse/scvi-tools/blob/7effcfb21468d272809c3bec85fea05aa9740db9/src/scvi/module/_utils.py#L3
@@ -26,3 +29,16 @@ def reparameterize(mu, logvar):
     std = torch.exp(0.5 * logvar)
     sample = mu + torch.randn_like(std) * std
     return mu, logvar, sample
+
+
+def evaluate(target_array, pred_proba_mat, target_name_mapper=None):
+    performance = defaultdict(dict)
+    
+    for ct in np.unique(target_array):
+        target = (target_array==ct).astype(int)
+        pred_proba = pred_proba_mat[..., ct]
+        ct_name = ct if target_name_mapper is None else target_name_mapper[ct]
+        performance["auprc"][ct_name] = average_precision_score(target, pred_proba)
+        performance["auroc"][ct_name] = roc_auc_score(target, pred_proba)
+
+    return pd.DataFrame(performance)
